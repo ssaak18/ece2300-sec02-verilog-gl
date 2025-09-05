cf599d870449d1b85aa1282c3c042c457383163c
sls552 <sls552@cornell.edu>
2025-09-05 14:02:54 -0400

    finished implementation

--- PairTripleDetector_GL.v
+++ PairTripleDetector_GL.v
@@ -18,11 +18,13 @@ module PairTripleDetector_GL
   //''' ACTIVITY '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
   // Implement pair/triple detector using explicit gate-level modeling
   //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-
-  `ECE2300_UNUSED( in0 );
-  `ECE2300_UNUSED( in1 );
-  `ECE2300_UNUSED( in2 );
-  `ECE2300_UNDRIVEN( out );
+  wire x;
+  wire y;
+  wire w;
+  or(w, in0, in1);
+  and(x, in0, in1);
+  and(y, w, in2);
+  or(out, y, x);
 
 endmodule
 
